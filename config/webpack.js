1cab32433e02ffb0dbe723fd8b1c2affb6c84fce
Qi Mu <skycloud112@gmail.com>
2016-10-14 16:16:45 -0400

    rename first js file

--- config/webpack.js
+++ config/webpack.js
@@ -1,7 +1,7 @@
 var path = require('path');
 
 module.exports = {
-  entry: path.resolve(__dirname, '../src/client/scripts/client.js'),
+  entry: path.resolve(__dirname, '../src/client/scripts/script.js'),
   output: {
     path: path.resolve(__dirname, '../dist'),
     filename: 'bundle.js'
