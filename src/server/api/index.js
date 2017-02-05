45c99e0006167f4bbad2b2b70334040a0d614747
stevemu <steve.mu.dev@gmail.com>
2017-02-04 19:25:03 -0500

    first commit

--- src/server/api/index.js
+++ src/server/api/index.js
@@ -3,11 +3,31 @@
 var express = require('express');
 var router = express.Router();
 
-// products
-
-router.get('/test', function(req, res) {
-  console.log(req.body);
-  res.json({test:"success"});
+router.get('/music_albums', function(req, res) {
+  const albums = [
+    {
+      title: 'taylor swift',
+      artist: 'taylor swift',
+      url: 'https://www.amazon.com/Fearless-Taylor-Swift/dp/B006WWU8IO/ref=sr_1_5?ie=UTF8&qid=1486253336&sr=8-5&keywords=taylor+swift',
+      image: "https://images-na.ssl-images-amazon.com/images/I/51g0OT%2BvfDL._SS500_PJStripe-Robin-Large,TopLeft,0,0.jpg",
+      thumbnail_image: 'https://images-na.ssl-images-amazon.com/images/I/51g0OT%2BvfDL._SS500_PJStripe-Robin-Large,TopLeft,0,0.jpg'
+    },
+    {
+      title: 'Speak Now',
+      artist: 'taylor swift',
+      url: 'https://www.amazon.com/Speak-Now-Taylor-Swift/dp/B0048IMZMG/ref=ntt_mus_dp_dpt_4',
+      image: "https://images-na.ssl-images-amazon.com/images/I/61cWjuYJyvL._SS500_PJStripe-Robin-Large,TopLeft,0,0.jpg",
+      thumbnail_image: 'https://images-na.ssl-images-amazon.com/images/I/61cWjuYJyvL._SS500_PJStripe-Robin-Large,TopLeft,0,0.jpg'
+    },
+   {
+      title: 'Bad Blood',
+      artist: 'taylor swift',
+      url: 'https://www.amazon.com/Bad-Blood-Taylor-Swift/dp/B00XTIFCXU/ref=ntt_mus_dp_dpt_9',
+      image: "https://images-na.ssl-images-amazon.com/images/I/51%2BoQX02R%2BL._SS500.jpg",
+      thumbnail_image: 'https://images-na.ssl-images-amazon.com/images/I/51%2BoQX02R%2BL._SS500.jpg'
+    },
+  ];
+  res.json(albums);
 });
 
 module.exports = router;
--- src/server/index.js
+++ src/server/index.js
@@ -1,5 +1,4 @@
 require('babel-core/register');
 require("babel-polyfill");
 
-
 module.exports = require('./server');
