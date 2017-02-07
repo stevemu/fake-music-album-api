b498bb21728dbd9475736295af86314ac9589f38
stevemu <steve.mu.dev@gmail.com>
2017-02-07 10:05:40 -0500

    use beales

--- src/server/api/index.js
+++ src/server/api/index.js
@@ -6,25 +6,25 @@ var router = express.Router();
 router.get('/music_albums', function(req, res) {
   const albums = [
     {
-      title: 'taylor swift',
-      artist: 'taylor swift',
-      url: 'https://www.amazon.com/Fearless-Taylor-Swift/dp/B006WWU8IO/ref=sr_1_5?ie=UTF8&qid=1486253336&sr=8-5&keywords=taylor+swift',
-      image: "https://images-na.ssl-images-amazon.com/images/I/51g0OT%2BvfDL._SS500_PJStripe-Robin-Large,TopLeft,0,0.jpg",
-      thumbnail_image: 'https://images-na.ssl-images-amazon.com/images/I/51g0OT%2BvfDL._SS500_PJStripe-Robin-Large,TopLeft,0,0.jpg'
+      title: 'Abbey Road',
+      artist: 'The Beatles',
+      url: 'https://www.amazon.com/Abbey-Road-Beatles/dp/B01929HGH4/ref=sr_1_2?ie=UTF8&qid=1486479679&sr=8-2&keywords=beatles',
+      image: "https://images-na.ssl-images-amazon.com/images/I/61i4ik3IBjL._SS500_PJStripe-Robin-Large,TopLeft,0,0.jpg",
+      thumbnail_image: 'https://images-na.ssl-images-amazon.com/images/I/61i4ik3IBjL._SS500_PJStripe-Robin-Large,TopLeft,0,0.jpg'
     },
     {
-      title: 'Speak Now',
-      artist: 'taylor swift',
-      url: 'https://www.amazon.com/Speak-Now-Taylor-Swift/dp/B0048IMZMG/ref=ntt_mus_dp_dpt_4',
-      image: "https://images-na.ssl-images-amazon.com/images/I/61cWjuYJyvL._SS500_PJStripe-Robin-Large,TopLeft,0,0.jpg",
-      thumbnail_image: 'https://images-na.ssl-images-amazon.com/images/I/61cWjuYJyvL._SS500_PJStripe-Robin-Large,TopLeft,0,0.jpg'
+      title: '1',
+      artist: 'The Beatles',
+      url: 'https://www.amazon.com/1-Beatles/dp/B01929HAK2/ref=sr_1_4?ie=UTF8&qid=1486479679&sr=8-4&keywords=beatles',
+      image: "https://images-na.ssl-images-amazon.com/images/I/31i5mjTO0FL._SS500.jpg",
+      thumbnail_image: 'https://images-na.ssl-images-amazon.com/images/I/31i5mjTO0FL._SS500.jpg'
     },
    {
-      title: 'Bad Blood',
-      artist: 'taylor swift',
-      url: 'https://www.amazon.com/Bad-Blood-Taylor-Swift/dp/B00XTIFCXU/ref=ntt_mus_dp_dpt_9',
-      image: "https://images-na.ssl-images-amazon.com/images/I/51%2BoQX02R%2BL._SS500.jpg",
-      thumbnail_image: 'https://images-na.ssl-images-amazon.com/images/I/51%2BoQX02R%2BL._SS500.jpg'
+      title: 'Rubber Soul',
+      artist: 'The Beatles',
+      url: 'https://www.amazon.com/Rubber-Soul-Beatles/dp/B01929HXIG/ref=sr_1_8?ie=UTF8&qid=1486479679&sr=8-8&keywords=beatles',
+      image: "https://images-na.ssl-images-amazon.com/images/I/61spT89nxEL._SS500_PJStripe-Robin-Large,TopLeft,0,0.jpg",
+      thumbnail_image: 'https://images-na.ssl-images-amazon.com/images/I/61spT89nxEL._SS500_PJStripe-Robin-Large,TopLeft,0,0.jpg'
     },
   ];
   res.json(albums);
